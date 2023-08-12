c7c9fcbf6d2b147e61e9a090513290229f07700c
chitchanok <bowfriday@gmail.com>
2023-08-12 16:42:54 +0700

    page 54

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,13 @@
 let express = require('express')
 const app = express()
 
-app.get('/status', function(req, res) {
+app.get('/status', function (req, res) {
   res.send('Hello nodejs server belong to me!')
+})
+
+app.get('/hello/:name', function (req, res) {
+  console.log('hello - ' + req.params.name)
+  res.send('say hello with ' + req.params.name)
 })
 
 let port = 8081
