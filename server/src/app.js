3de9f501142b444bcd3eecb26a11f260bd73dd87
chitchanok <bowfriday@gmail.com>
2023-08-16 09:56:59 +0700

    finish 4

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,10 @@
 let express = require('express')
+let bodyParser = require('body-parser')
+
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get('/status', function (req, res){
   res.send('Hello nodejs server belong to me!')
@@ -10,6 +15,25 @@ app.get('/hello/:name', function (req, res) {
   res.send('Say hello with ' + req.params.name)
 })
 
+app.get('/user/:userId', function (req, res) {
+  res.send('ดูข้อมูลผู้ใช้ '+ req.params.userId)
+})
+
+app.get('/users', function (req, res) {
+  res.send('ดูข้อมูลผู้ใช้ทั้งหมด')
+})
+
+app.post('/user/', function (req, res) {
+  res.send('สร้างผู้ใช้: ' + JSON.stringify(req.body))
+})
+
+app.put('/user/:userId', function(req, res) {
+  res.send('แก้ไขข้อมูลผู้ใช้ ' + req.params.userId + " " + JSON.stringify(req.body))
+})
+
+app.delete('/user/:userId', function (req, res) {
+  res.send('ลบข้อมูลผู้ใช้ ' + req.params.userId + " " + JSON.stringify(req.body))
+})
 let port = 8081
 app.listen(port,function(){
     console.log('server running on ' + port)
