7f717e6f57035f0007ec0190fb0319c385efec54
chitchanok <bowfriday@gmail.com>
2023-08-16 10:40:27 +0700

    ch05

---
+++ server/src/route.js
@@ -0,0 +1,14 @@
+const UserController = require('./controllers/UserController')
+
+module.exports = (app) => {
+  app.get('/users', UserController.index),
+
+  app.post('/user', UserController.create),
+
+  app.put('/user/:userId', UserController.put),
+
+  app.delete('/user/:userId', UserController.delete),
+
+  app.get('/user/:userId', UserController.show)
+
+}
